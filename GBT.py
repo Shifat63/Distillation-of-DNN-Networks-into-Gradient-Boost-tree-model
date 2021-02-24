18cb98689b7f7a608d53427bca16e19c8b80df17
haque <s6mdhaqu@uni-bonn.de>
2021-02-24 18:11:36 +0100

    Documentation added

--- GBT.py
+++ GBT.py
@@ -9,12 +9,51 @@ maxTreeDepth = 3
 
 #Train the model
 def trainXGbtClassification(X, y):
+    '''
+    objective function = "multi:softprob" (Used for multiclass classification)
+    Maximum number of trees = 100
+    Learning rate = 0.1
+    Maximum tree depth = 3
+
+    For first pipeline:
+    We pass the training inputs and generated soft labels from the NN to
+    this function to train the Gradient Boosting Tree(GBT).
+    For second pipeline:
+    We pass the training inputs and generated soft labels from the Helper classifier to
+    this function to train the Gradient Boosting Tree(GBT).
+
+    Parameters
+    ----------
+    X : Training inputs
+    y : Soft labels
+
+    Returns
+    -------
+    model : Trained GBT model
+
+    '''
     model = XGBClassifier(learning_rate=learningRate, n_estimators=maxNumberOfTrees, max_depth=maxTreeDepth)
     model.fit(X, y)
     return model
 
 #Test accuracy of the model
 def testGbt(model, X, y):
+    '''
+    After we have trained our GBT model, we are calculating the accuracy of
+    trained model on the test dataset - percentage of samples that are
+    classified correctly.
+
+    Parameters
+    ----------
+    model : trained GBT model
+    X : Test inputs
+    y : Desired(actual) output for the given test inputs
+
+    Returns
+    -------
+    acc : model accuracy on test dataset
+
+    '''
     predictions = model.predict(X)
     for i in range(len(predictions)):
         predictions[i] = round(predictions[i])
@@ -23,6 +62,21 @@ def testGbt(model, X, y):
 
 #Show GBT trees. Last one from each block
 def showTree(model, blockSize):
+    '''
+    Show GBT trees. Last tree from each block.
+    To save computational power all the trees generated are not shown.
+    If it is required to show all trees then provide blockSize=1
+
+    Parameters
+    ----------
+    model : trained GBT model
+    blockSize : size of block from which last tree will be shown
+
+    Returns
+    -------
+    None.
+
+    '''
     for i in range(int(maxNumberOfTrees/blockSize)):
         plot_tree(model, num_trees=i*blockSize+(blockSize-1))
         plt.show()
--- NN.py
+++ NN.py
@@ -14,7 +14,22 @@ import matplotlib.pyplot as plt
 # model definition
 class MLP(Module):
     # define model elements
+    '''
+    Define a class that extends Module class.
+    '''
     def __init__(self, n_inputs):
+        '''
+        The constructor of MLP class defines the layers. We define MLP
+        with input layer(n_inputs = 13 neurons), first hidden layer(26 neurons),
+        second hidden layer(13 neurons) and output layer. That gives us 689
+        weights to be adjusted. We are using Kaiming for the weight initialisation
+        strategy for hidden1 - > hidden2, hidden2 - > output layer, according to the
+        fact that we are using ReLU() activation function for the both hidden layers.
+        For the output layer, we are using Sigmoid() activation function, suitable
+        for our binary classification task. We are using Xavier initialization for
+        the weights from hidden2 -> output, because it can solve Sigmoid() vanishing
+        gradient problem.
+        '''
         super(MLP, self).__init__()
         # input to first hidden layer
         self.hidden1 = Linear(n_inputs, 26)
@@ -33,6 +48,21 @@ class MLP(Module):
 
     # forward propagate input
     def forward(self, X):
+        '''
+        This function takes the input data (rows of heart.csv, without original
+        target labels). The input data is fed in the forward direction through
+        the network. Each hidden layer accepts the input data, processes it, as per
+        the activation function and passes to the successive layer.
+
+        Parameters
+        ----------
+        X : input values from heart.csv dataset (without the last "target column")
+
+        Returns
+        -------
+        X : calculated output(target) value for the given input
+
+        '''
         # input to first hidden layer
         X = self.hidden1(X)
         X = self.act1(X)
@@ -47,6 +77,23 @@ class MLP(Module):
         return X
     
     def forwardLastHiddenLayer(self, X):
+        '''
+        This function takes the input data (rows of heart.csv, without original
+        target labels). The input data is fed in the forward direction through
+        the network. Each hidden layer accepts the input data, processes it, as per
+        the activation function and passes to the successive layer. This function
+        returns the output of the second hidden layer. The goal is to extract the
+        learned features from the last hidden layer (in our case - second hidden layer)
+
+        Parameters
+        ----------
+        X : input values from heart.csv dataset (without the last "target column")
+
+        Returns
+        -------
+        X : learned features from the second hidden layer
+
+        '''
         # input to first hidden layer
         X = self.hidden1(X)
         X = self.act1(X)
@@ -57,6 +104,29 @@ class MLP(Module):
     
 # train the model
 def train_model(train_dl, test_dl, model):
+    '''
+    For training of defined MLP model, we have to define loss function
+    and optimization algorithm that will be used. Binary cross entropy
+    loss is used as loss function. Stochastic gradient descent is used
+    for optimization. SGD class provides standard algorithm. In the outer
+    loop, we are defining the number of training epochs. In each epoch,
+    the inner loop is required for enumerating the mini batches for SGD.
+    Each update of the model consists of the following steps: clear the
+    gradients, feed the inputs to the network, calculate loss, backpropagate
+    the error through the network, update model weights.Additionaly, this
+    function plots training and validation learning curves.
+
+    Parameters
+    ----------
+    train_dl : training dataset
+    test_dl : test dataset
+    model : object of MLP class
+
+    Returns
+    -------
+    None.
+
+    '''
     # define the optimization
     criterion = BCELoss()
     optimizer = SGD(model.parameters(), lr=0.005, momentum=0.9)
@@ -98,6 +168,22 @@ def train_model(train_dl, test_dl, model):
 
 # Generate soft labels
 def get_soft_labels(data, model):
+    '''
+    For implementing the first pipeline, we are using predicted soft labels
+    for GBT training. This function returns predicted soft labels, along
+    with inputs, we keep track on the inputs for which we make predictions,
+    along with the target(original) labels.
+    Parameters
+    ----------
+    data : training dataset
+    model : object of MLP class
+    Returns
+    -------
+    xinputs : inputs in the order in which we calculate predictions
+    predictions : soft labels, without rounding
+    true : original (true) target values
+
+    '''
     xinputs, predictions, true = [], [], []
     for i, (inputs, targets) in enumerate(data):
         yhat = model(inputs)
@@ -111,6 +197,26 @@ def get_soft_labels(data, model):
 # oinputs: contain original inputs
 # true: the true labels
 def get_last_layer(data, model):
+    '''
+    For implementing the second pipeline, we are using logistic regression
+    as helper classifier. We are extracting activations from the last hidden
+    layer and feed them into the helper classifier to predict the original
+    task. This function returns the activations from the last hidden layer.
+    We keep track on the inputs for which we extract activations, along with
+    the target(original) labels.
+
+    Parameters
+    ----------
+    data : training dataset
+    model : object of MLP class
+
+    Returns
+    -------
+    xinputs : activations from the last hidden layer
+    oinputs : inputs in the order in which we calculate activations
+    true : original (true) target values
+
+    '''
     xinputs, true, oinputs = [], [], []
     for i, (inputs, targets) in enumerate(data):
         yhat = model.forwardLastHiddenLayer(inputs)
@@ -122,6 +228,21 @@ def get_last_layer(data, model):
 
 # evaluate the model
 def evaluate_model(test_dl, model):
+    '''
+    After we have trained our model, we are calculating the accuracy of
+    trained model on the test dataset - percentage of samples that are
+    classified correctly.
+
+    Parameters
+    ----------
+    test_dl : test dataset
+    model : object of MLP class
+
+    Returns
+    -------
+    acc : model accuracy on test dataset
+
+    '''
     predictions, actuals = list(), list()
     for i, (inputs, targets) in enumerate(test_dl):
         # evaluate the model on the test set
